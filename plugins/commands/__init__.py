60ac41bc9ef46f1afa9845a4b0568aedb74c5a19
wagner-austin <austinwagner@msn.com>
2025-03-07 21:27:49 -0800

    Alphabetize imports in plugins/commands/__init__.py to include all command modules

--- plugins/commands/__init__.py
+++ plugins/commands/__init__.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python
 """
 plugins/commands/__init__.py - Package initialization for command plugins.
-This module imports and re-exports commands from submodules.
+This module imports and re-exports commands from submodules in alphabetical order.
 """
 
-from .system import *
 from .event import *
-from .volunteer import *
 from .help import *
-from .speaker import *
+from .political import *
 from .resource import *
+from .role import *
+from .speaker import *
+from .system import *
+from .theme import *
+from .volunteer import *
 
 # End of plugins/commands/__init__.py
